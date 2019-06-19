8b5385609fe079d2acfba9b66a22bb3e3bae831f
pepo <pepo@ukr.net>
2019-06-19 13:05:23 +0300

    createremove

--- main.js
+++ main.js
@@ -399,11 +399,20 @@ let elem1=document.querySelector('.div-2');
         div2.remove(); */
 
         let div1=document.querySelector('.div-1');
-        let inp=document.querySelector('.inp');
-        let div2=document.createElement('div');
-        div2.innerHTML='Hello World';
+        let inp=document.querySelector('.but');
         inp.addEventListener('click', function(){
-            
+            let div2=document.createElement('div');
+            div1.appendChild(div2);
+            div2.innerHTML='hello';
+            div2.className='nub';
+            let input2=document.createElement('input');
+            div1.appendChild(input2);
+            input2.type='button';
+            input2.value='-';
+            input2.addEventListener('click', function(){
+                div2.remove();
+                input2.remove();
+            })
         })
         
 
